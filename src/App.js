33e2a9a2060d03399ade93e84f5b4e48355e7931
Jon Mullingtons <jmulhern85@gmail.com>
2022-03-10 17:14:03 +0000

    Added header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from "./logo.svg";
-import "./App.css";
-import "./Piano.css";
+
+import "./css/App.css";
+import "./css/Buttons.css";
+import "./css/Header.css";
+import "./css/Piano.css";
+import chordsLogo from "./media/chords.png"
+
 import { newChord } from "./js/onclick";
 import { newPiano } from "./js/piano.js";
 import React, { useEffect, useState, Component } from "react";
@@ -14,11 +19,18 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <div id="pianoContainer"></div>
-      <button id="newChordButton" onClick={() => newChord(piano)}>
-        Pick a new chord
-      </button>
+      <header className="App-header">
+        <span class="material-icons">home</span>
+        <img src={chordsLogo} alt="Piano chord test" />
+        <span class="material-icons">settings</span>
+      </header>
+      <div id="contentBody">
+        <div id="pianoContainer"></div>
+        <button id="newChordButton" onClick={() => newChord(piano)}>
+          Pick a new chord
+        </button>
+        <div id="answerButtonsContainer"></div>
+      </div>
     </div>
   );
 }
